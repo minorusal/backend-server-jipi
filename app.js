6873c0a17054f07cf22c405c096c8cae73c0d3b5
minorusal <minorusal@hotmail.com>
2018-10-27 01:05:22 -0500

    Seccion 11 terminada - CRUD - Imagenes

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(express.json());
 // Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 var loginRoutes = require('./routes/login');
 
 // Conexión a la base de datos
@@ -27,9 +32,19 @@ mongoose.connection.openUri('mongodb://localhost:27017/jipidb', { useNewUrlParse
 });
 mongoose.set('useCreateIndex', true);
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
 app.use('/', appRoutes);
 
 // Escuchar peticiones

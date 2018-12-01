af222344baaae70b5324fe3095878220432b7ab9
minorusal <minorusal@hotmail.com>
2018-12-01 12:43:01 -0600

    ejemplo pdf

--- routes/app.js
+++ routes/app.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
 var Usuario = require('../models/usuario');
+
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = fs.readFileSync('../pdfTest/test.html', 'utf8');
+var options = { format: 'Letter' };
+
 app.get('/', (req, res, next) => {
+    pdf.create(html, options).toFile('./test.pdf', function(err, res) {
+        if (err) return console.log(err);
+        console.log(res); // { filename: '/app/businesscard.pdf' }
+    });
     res.status(200).json({
         ok: true,
         mensaje: 'Petición realizada correctamente'
